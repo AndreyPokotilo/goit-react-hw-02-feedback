256c319f6599a423452d41b675014fe93157a77d
AndreyPokotilo <102250174+AndreyPokotilo@users.noreply.github.com>
2023-04-29 22:29:25 +0200

    added all tasks

---
+++ src/components/FeedbackOptions/FeedbackOptions.styled.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const BtnList = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  max-width: 220px;
+  margin: 0 auto;
+  list-style-type: none;
+`;
+
+export const BtnItem = styled.li`
+  text-align: center;
+`;
+
+export const Button = styled.button`
+  font-size: 16px;
+  padding: 4px;
+  width: 70px;
+  cursor: pointer;
+  background-color: aqua;
+`;
